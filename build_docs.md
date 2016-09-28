4327f2df2f660bc1934ff17aa94ae1c1427a1b43
peb7268 <peb7268@gmail.com>
2016-09-28 17:58:46 -0400

    wip

---
+++ build_docs.md
@@ -0,0 +1,5 @@
+//Start browser sync
+browser-sync start --server --files "*.html, css/*.css"
+
+//open a new terminal tab and run gulp
+gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-	//.pipe(livereload())
-    return gulp.src('./sass/*.scss')
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        //server: "./app"
+         baseDir: "./"
+    });
+});
+
+gulp.task('sass', function() {
+    gulp.src("sass/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+
+    gulp.watch("sass/*.scss", ['sass']).on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass']);
